d3ad64e57bebcb3e1bb8f601dd5fca28204311c5
prashanthrangineni-sketch <prashanthrangineni@gmail.com>
2026-02-11 21:24:55 +0530

    Update page.tsx

--- app/set-session/page.tsx
+++ app/set-session/page.tsx
@@ -1,34 +1,12 @@
-'use client'
+import { Suspense } from 'react'
+import SetSessionClient from './set-session-client'
 
-// 👇 THIS LINE FIXES THE BUILD ERROR
 export const dynamic = 'force-dynamic'
 
-import { useEffect } from 'react'
-import { useSearchParams, useRouter } from 'next/navigation'
-
 export default function SetSessionPage() {
-  const params = useSearchParams()
-  const router = useRouter()
-
-  useEffect(() => {
-    const sid = params.get('sid')
-
-    if (!sid) {
-      alert('Missing session id')
-      return
-    }
-
-    // Store session for basket page
-    localStorage.setItem('cart2save_session', sid)
-
-    // Go to basket
-    router.replace('/basket')
-  }, [params, router])
-
   return (
-    <div style={{ padding: 20 }}>
-      Setting session…
-    </div>
+    <Suspense fallback={<div style={{ padding: 20 }}>Setting session…</div>}>
+      <SetSessionClient />
+    </Suspense>
   )
 }
-``
