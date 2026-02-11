6c2158f7d9c28abb2028c643c87a5f4ffd3e3116
prashanthrangineni-sketch <prashanthrangineni@gmail.com>
2026-02-11 10:08:26 +0530

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,14 +1,13 @@
+// lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
-
 export const supabase = createClient(
-  supabaseUrl,
-  supabaseServiceKey,
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   {
     auth: {
-      persistSession: false
+      persistSession: true,
+      autoRefreshToken: true
     }
   }
 )
