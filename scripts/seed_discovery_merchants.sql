3ebdd589c31b711549dd0951f404331ca451a18a
prashanthrangineni-sketch <prashanthrangineni@gmail.com>
2026-02-28 21:32:15 +0530

    Create seed_discovery_merchants.sqlchore: add discovery seed reference sql

---
+++ scripts/seed_discovery_merchants.sql
@@ -0,0 +1,5 @@
+-- Discovery Seed Example
+INSERT INTO offers (title, price, affiliate_network, sector)
+VALUES
+('Sample Grocery Product', 120, 'discovery', 'grocery'),
+('Sample Electronics Product', 999, 'discovery', 'electronics');
