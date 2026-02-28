e99225c0424c111efb949f59b736d8f5efa168d8
prashanthrangineni-sketch <prashanthrangineni@gmail.com>
2026-02-28 21:03:35 +0530

    chore: allow amazon image domains for product rendering

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'm.media-amazon.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images-na.ssl-images-amazon.com',
-      },
+      { protocol: 'https', hostname: 'm.media-amazon.com' },
+      { protocol: 'https', hostname: 'images-na.ssl-images-amazon.com' },
+      { protocol: 'https', hostname: '*.ssl-images-amazon.com' },
     ],
   },
 };
